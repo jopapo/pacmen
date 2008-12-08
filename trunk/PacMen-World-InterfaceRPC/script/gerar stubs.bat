e68da40c7675496730834300f6390254370496aa
jopapo <jopapo@b952335c-b4f3-11dd-98dc-370f6699448a>
2008-12-08 10:51:56 +0000

    * Implementando módulo world.

---
+++ trunk/PacMen-World-InterfaceRPC/script/gerar stubs.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo Acessando diretorio...
+cd "C:\arquivo\furb\materias\2008-2\sd\trab-final\PacMen\PacMen-World-InterfaceRPC\src\br\pacmen\world\rpc\comm"
+echo Excluindo stubs antigos...
+del *.java /Q
+echo Gerando novos stubs...
+"C:\arquivo\furb\materias\2008-2\sd\trab-final\PacMen\PacMen-World-InterfaceRPC\script\jrpcgen" ..\PacMen.x
+
+echo Concluido!
+pause
